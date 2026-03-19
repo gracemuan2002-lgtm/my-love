bfdedfbc74715a5a8cd0792ddbbeb5b02534fe5c
mang muan <mangmuan>
2026-03-19 22:44:46 +0800

    second

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,29 @@ import { useState } from 'react'
 
 
 const App = () => {
-  const [noPosition, setNoPosition] = useState({x:250,y:384});
+  const [noPosition, setNoPosition] = useState({x:230,y:454});
   const moveNO = (e) => {
-    const button = e.target;
-    const rect= button.getBoundingClientRect();
-    const cursorX = e.clientX;
-    const cursorY = e.clientY;
-    const distanceX= Math.abs(cursorX - (rect.left + rect.width / 2));
-    const distanceY= Math.abs(cursorY - (rect.top + rect.height / 2));
-    const threshold = 100;
-    if (distanceX < threshold && distanceY < threshold) {
-      const containerWidth =400;
-      const containerHeight =300;
-      const buttonWidth = rect.width;
-      const buttonHeight = rect.height;
-      const maxX = containerWidth - buttonWidth;
-      const maxY = containerHeight - buttonHeight;
-      const newX = Math.random() * maxX;
-      const newY = Math.random() * maxY;
-      setNoPosition({x:newX,y:newY});
+    let clientX, clientY;
+    if (e.touches) {
+      clientX = e.touches[0].clientX;
+      clientY = e.touches[0].clientY;
+    } else {
+      clientX = e.clientX;
+      clientY = e.clientY;
     }
+    const rect = e.target.getBoundingClientRect();
+    const btnCenterX = rect.left + rect.width / 2;
+    const btnCenterY = rect.top + rect.height / 2;
+    const distance =Math.hypot(clientX - btnCenterX, clientY - btnCenterY);
+    const threshold = 100;
+    if (distance <threshold) {
+      const containerWidth = 360;
+      const containerHeight = 640;
+      const x = Math.random() * (containerWidth - rect.width);
+      const y = Math.random() * (containerHeight - rect.height);
+      setNoPosition({ x, y });
+    }
+    
   }
   const handleyes = () => {
     const name = prompt("Enter your name:");
@@ -37,12 +40,14 @@ const App = () => {
   }
   return (
     <div className='flex justify-center items-center'>
-      <div className='bg-pink-200 w-[400px] h-[500px]  text-center  relative border rounded-lg flex flex-col justify-center items-center gap-10 '>
+      <div className='bg-pink-200 w-[360px] h-[640px]  text-center  relative border rounded-lg flex flex-col justify-center items-center gap-10 '>
         <img src={image.image6} alt='couple' className='w-[200px] h-[200px] rounded-full'/>
         <div className='text-2xl font-black'>Do you love me?</div>
         <div className='flex gap-4 text-lg font-bold'>
           <button onClick={handleyes}className='bg-blue-500 text-white px-4 py-2 rounded'>YES</button>
-          <button onMouseEnter={moveNO} style={{
+          <button onMouseEnter={moveNO}
+            onTouchMove={moveNO}
+            style={{
             position:"absolute",
             left:noPosition.x,
             top:noPosition.y
