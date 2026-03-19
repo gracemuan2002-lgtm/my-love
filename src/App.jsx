bbdca643ec2549e78eff1a28de11331a8b76bc55
mang muan <mangmuan>
2026-03-19 23:56:55 +0800

    second

--- src/App.jsx
+++ src/App.jsx
@@ -40,12 +40,14 @@ const App = () => {
   }
   return (
     <div className='flex justify-center items-center'>
-      <div className='bg-pink-200 w-screen h-screen  text-center  relative border rounded-lg flex flex-col justify-center items-center gap-10  onTouchStart={moveNo}'>      
+      <div className='bg-pink-200 w-screen h-screen  text-center  relative border rounded-lg flex flex-col justify-center items-center gap-10  '>      
         <img src={image.image6} alt='couple' className='w-[200px] h-[200px] rounded-full'/>
         <div className='text-2xl font-black'>Do you love me?</div>
         <div className='flex gap-4 text-lg font-bold'>
           <button onClick={handleyes}className='bg-blue-500 text-white px-4 py-2 rounded'>YES</button>
           <button onMouseEnter={moveNO}
+            onTouchStart={moveNo}
+            onClick={moveNo}
             style={{
             position:"absolute",
             left:noPosition.x,
